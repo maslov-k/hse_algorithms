e5b4fc61f070f6c77d1511d03056100947a08cc7
Kirill Maslov <ksmaslov@mail.ru>
2024-09-25 16:38:20 +0300

    add divide and conquer solution for leetcode 53

--- 3__leetcode_53.py
+++ 3__leetcode_53.py
@@ -17,10 +17,29 @@ class Solution:
         return result
 
 
+class SolutionDC:
+    def maxSubArray(self, nums: List[int]) -> int:
+        pre, suf = [*nums], [*nums]
+        for i in range(1, len(nums)):       pre[i] += max(0, pre[i - 1])
+        for i in range(len(nums) - 2, -1, -1):  suf[i] += max(0, suf[i + 1])
+
+        def maxSubArray(a, left, right):
+            if left == right:
+                return a[left]
+
+            mid = (left + right) // 2
+
+            return max(maxSubArray(a, left, mid), maxSubArray(a, mid + 1, right), pre[mid] + suf[mid + 1])
+
+        return maxSubArray(nums, 0, len(nums) - 1)
+
+
 if __name__ == '__main__':
     solution = Solution()
+    solutionDc = SolutionDC()
 
     print(solution.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+    print(solutionDc.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
 
 
 
